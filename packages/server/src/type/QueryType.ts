13ea09f4e95594479991893b432396d4bd3b9917
Renan Machado <renan.mav@hotmail.com>
2019-09-12 17:28:57 -0300

    add description to queries

--- packages/server/src/type/QueryType.ts
+++ packages/server/src/type/QueryType.ts
@@ -11,25 +11,22 @@ export default new GraphQLObjectType({
   fields: () => ({
     githubLoginUrl: {
       type: GraphQLString,
+      description: 'Use this query to fetch GitHub login URL',
       resolve: () =>
-        `https://github.com/login/oauth/authorize?client_id=${
-          process.env.GITHUB_CLIENT_ID
-        }&scope=user`
+        `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user`
     },
     node: nodeField,
     me: {
       type: UserType,
+      description: 'Get your own data based on provided Authorization token',
       resolve: (_, __, { user }: GraphQLContext) => user
     },
     user: {
       type: UserType,
+      description: 'Fetch a user by its id or login',
       args: {
-        id: {
-          type: GraphQLString
-        },
-        login: {
-          type: GraphQLString
-        }
+        id: { type: GraphQLString },
+        login: { type: GraphQLString }
       },
       resolve: (_, args, context) => {
         if (args.id) {
