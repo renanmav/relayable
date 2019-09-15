0ffc70a8be148b4641be044ddf9117fb2bf7a51c
Renan Machado <renan.mav@hotmail.com>
2019-09-15 13:16:42 -0300

    config for Heroku deploy

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -2,12 +2,9 @@ const { env } = process
 
 const dbProd = env.MONGODB_URL
 const dbDev = `mongodb://${
-  env.IS_FROM_DOCKER
-    ? 'database'
-    : 'localhost'
+  env.IS_FROM_DOCKER ? 'database' : 'localhost'
 }:27017/db-dev`
 
-export const graphqlPort = env.GRAPHQL_PORT || 5000
-export const dbUri = env.NODE_ENV === 'production' || env.IS_FROM_NOW
-  ? dbProd
-  : dbDev
+export const graphqlPort = env.PORT || 5000
+export const dbUri =
+  env.NODE_ENV === 'production' || env.IS_NOT_LOCAL ? dbProd : dbDev
