7c14f5f87905120a471ef9c9825b8300d6ac12e3
Renan Machado <renan.mav@hotmail.com>
2019-09-23 16:07:19 -0300

    perform LoginWithGithub mutation

--- packages/web/src/pages/Auth/index.tsx
+++ packages/web/src/pages/Auth/index.tsx
@@ -1,13 +1,30 @@
 import React, { useEffect } from 'react'
 import { RouteComponentProps } from '@reach/router'
 import { parse } from 'query-string'
+import { yottaToken, yottaUser } from '@yotta/web/src/utils/contants'
 
-const Auth: React.FC<RouteComponentProps> = ({ location }) => {
+import LoginWithGithubMutation from './mutation/LoginWithGithubMutation'
+
+const Auth: React.FC<RouteComponentProps> = ({ location, navigate }) => {
   const { code } = parse(location!.search)
 
   useEffect(() => {
     if (!code) return
-    console.log(code)
+
+    LoginWithGithubMutation.commit(
+      { code: code as string },
+      response => {
+        if (!response!.LoginWithGithub!.token) return
+
+        const { token, user } = response!.LoginWithGithub!
+
+        localStorage.setItem(yottaToken, token!)
+        localStorage.setItem(yottaUser, JSON.stringify(user))
+
+        navigate!('/')
+      },
+      error => console.log(error)
+    )
   }, [])
 
   return <div />
