cdae7f3007291247888dc1c690371575494f8857
Renan Machado <renan.mav@hotmail.com>
2019-09-29 17:41:59 -0300

    change Main to relay hooks

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-// @ts-ignore
 import { RelayEnvironmentProvider } from '@entria/relay-experimental'
 import { Environment } from '@yotta/web/src/relay'
-
-import CssBaseline from '@material-ui/core/CssBaseline'
 
 import Main from './pages/Main'
 
 render(
   <RelayEnvironmentProvider environment={Environment}>
-    <CssBaseline />
     <Main />
   </RelayEnvironmentProvider>,
   document.getElementById('app')
--- packages/web/src/pages/Main/index.tsx
+++ packages/web/src/pages/Main/index.tsx
@@ -1,46 +1,34 @@
 import React from 'react'
-import { createFragmentContainer, graphql, RelayProp } from 'react-relay'
+import { graphql } from 'react-relay'
 
 import ThemeProvider from '@material-ui/styles/ThemeProvider'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
-import { createQueryRendererModern } from '@yotta/web/src/relay'
 import Routes from '@yotta/web/src/routes'
 
+import { useQuery } from '@entria/relay-experimental'
+
 import { darkTheme, lightTheme } from './styles'
-import { Main_query } from './__generated__/Main_query.graphql'
+import { MainQuery } from './__generated__/MainQuery.graphql'
 
-interface MainProps {
-  query: Main_query
-  relay: RelayProp
-}
-
-const Main: React.FC<MainProps> = ({
-  query: {
+const Main: React.FC<{}> = () => {
+  const {
     settings: { darkTheme: isDark },
-  },
-}) => (
-  <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-    <Routes />
-    <CssBaseline />
-  </ThemeProvider>
-)
-
-const MainFragment = createFragmentContainer(Main, {
-  query: graphql`
-    fragment Main_query on Query {
+  } = useQuery<MainQuery>(graphql`
+    query MainQuery {
+      __typename
       settings {
         darkTheme
       }
     }
-  `,
-})
+  `)
 
-export default createQueryRendererModern(MainFragment, Main, {
-  query: graphql`
-    query MainQuery {
-      __typename
-      ...Main_query
-    }
-  `,
-})
+  return (
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Routes />
+    </ThemeProvider>
+  )
+}
+
+export default Main
