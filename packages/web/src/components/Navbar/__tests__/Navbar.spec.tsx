98759adba7e6c30a39e684e1c33b2066810ab140
Renan Machado <renan.mav@hotmail.com>
2019-10-02 16:12:32 -0300

    add @types/relay-test-utils

--- packages/web/src/components/Navbar/__tests__/Navbar.spec.tsx
+++ packages/web/src/components/Navbar/__tests__/Navbar.spec.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-// @ts-ignore
 import { MockPayloadGenerator } from 'relay-test-utils'
 import { env } from '@yotta/web/src/relay/createQueryRendererModern'
 
@@ -15,7 +14,7 @@ test('Loading state', () => {
 test('Data render', () => {
   const component = renderer.create(<Navbar />)
 
-  env.mock.resolveMostRecentOperation((operation: any) => MockPayloadGenerator.generate(operation))
+  env.mock.resolveMostRecentOperation(operation => MockPayloadGenerator.generate(operation))
 
   expect(component).toMatchSnapshot()
 })
