04092d875ed0413b0ca0dcbbffe0df449c684f1f
Renan Machado <renan.mav@hotmail.com>
2019-11-08 22:17:52 -0300

    feat(global): adjust tsconfig

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -4,6 +4,7 @@ import { render } from 'react-dom'
 import './reset.css'
 
 import { RelayEnvironmentProvider } from '@entria/relay-experimental'
+
 import { Environment } from '@relayable/web/src/relay'
 
 import Main from './modules/Main'
